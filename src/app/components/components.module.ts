d94301036ccffb864f1ddc691113744c434b0a79
Carles Ramos <carlesdeveloper@gmail.com>
2021-01-30 14:21:21 -0800

    example finished

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsRoutingModule } from './components-routing.module';
-import { ToolbarComponent } from './toolbar/toolbar.component';
+
 /* material */
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatTooltipModule} from '@angular/material/tooltip';
-import { SidemenuComponent } from './sidemenu/sidemenu.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatListModule} from '@angular/material/list';
 import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
-  declarations: [ToolbarComponent, SidemenuComponent],
+  declarations: [],
   imports: [
     CommonModule,
     ComponentsRoutingModule,
@@ -28,7 +27,7 @@ import {MatExpansionModule} from '@angular/material/expansion';
     MatExpansionModule
   ],
   exports: [
-    ToolbarComponent
+
   ]
 })
 export class ComponentsModule { }
